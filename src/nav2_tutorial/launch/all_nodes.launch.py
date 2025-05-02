042457593cf9e2c136ad35a4cc3c85b4942e44f3
Facundo Garcia <facundo.garcia@fixposition.com>
2025-05-02 12:44:41 +0000

    Update launch file

--- src/nav2_tutorial/launch/all_nodes.launch.py
+++ src/nav2_tutorial/launch/all_nodes.launch.py
@@ -3,7 +3,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, ExecuteProcess
-from launch.launch_description_sources import PythonLaunchDescriptionSource, XMLLaunchDescriptionSource
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
@@ -30,8 +30,8 @@ def generate_launch_description():
 
         # 3) Driver node
         IncludeLaunchDescription(
-            XMLLaunchDescriptionSource(
-               os.path.join(driver_pkg_share, 'fp_driver_node.launch')
+            PythonLaunchDescriptionSource(
+                os.path.join(driver_pkg_share, 'fp_driver_node.launch.py')
             ),
             launch_arguments={'config': 'config/fp_driver_config.yaml'}.items(),
         ),
